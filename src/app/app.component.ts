9a8e29221d69a871c7fae03be5e606e4be55b39c
ekalaya2015 <ekalaya2015@gmail.com>
2022-02-22 08:21:14 +0700

    onesignal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { OneSignal } from 'onesignal-ngx';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,7 +6,7 @@ import { OneSignal } from 'onesignal-ngx';
 })
 export class AppComponent {
   title = 'Emtres - Tax Monitoring';
-  constructor(private oneSignal: OneSignal){
-    this.oneSignal.init({appId:"3c668c69-f13b-4ea6-9593-dd41c7e3c724"})
+  constructor(){
+    
   }
 }
